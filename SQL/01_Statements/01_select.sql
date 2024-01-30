0e030695775378261654dac28468ba772561a97e
Jose Ricardez <121765994+JoseAlRic@users.noreply.github.com>
2024-01-30 16:49:43 -0600

    Update 01_select.sql

--- SQL/01_Statements/01_select.sql
+++ SQL/01_Statements/01_select.sql
@@ -3,7 +3,7 @@ SELECT Statement:
         La declaración SELECT se utiliza para seleccionar datos de una base de datos.
 
     Ejemplo
-        Datos de devolución de la tabla Clientes:*/
+        Datos de devolución de la tabla Customers:*/
         SELECT CustomerName, City FROM Customers;/*
 
     Sintaxis*/
@@ -17,11 +17,11 @@ Base de datos:
     A continuación se muestra una selección de la tabla Clientes utilizada en los ejemplos:
 
         CustomerID           CustomerName	                  ContactName         	    Address	                  City	             PostalCode	        Country
-        1                Alfreds Futterkiste	              Maria Anders	          Obere Str. 57	             Berlin	                12209	        Germany
-        2	    Ana Trujillo Emparedados y helados	          Ana Trujillo	    Avda. de la Constitución 2222	México D.F.	            05021	        Mexico
-        3	          Antonio Moreno Taquería	             Antonio Moreno	          Mataderos 2312	        México D.F.	            05023	        Mexico
-        4                   Around the Horn	                   Thomas Hardy	          120 Hanover Sq.	          London	           WA1 1DP	          UK
-        5	              Berglunds snabbköp	            Christina Berglund	      Berguvsvägen 8	          Luleå	               S-958 22	         Sweden
+        1                Alfreds Futterkiste	                  Maria Anders	          Obere Str. 57	                 Berlin	                12209	        Germany
+        2	    Ana Trujillo Emparedados y helados	         Ana Trujillo	    Avda. de la Constitución 2222	México D.F.	        05021	        Mexico
+        3	          Antonio Moreno Taquería	         Antonio Moreno	          Mataderos 2312	        México D.F.	        05023	        Mexico
+        4                   Around the Horn	                  Thomas Hardy	          120 Hanover Sq.	          London	        WA1 1DP	          UK
+        5	              Berglunds snabbköp	       Christina Berglund	   Berguvsvägen 8	          Luleå	                S-958 22	 Sweden
 
 Seleccione TODAS las columnas
     Si desea devolver todas las columnas, sin especificar cada nombre de columna, puede utilizar la sintaxis  SELECT *:
